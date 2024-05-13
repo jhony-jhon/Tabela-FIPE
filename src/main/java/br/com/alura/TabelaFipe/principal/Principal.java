9e31c9d39843802176c4102aa93b7df5334878da
Jhony Batista <telinabatista@gmail.com>
2024-05-13 11:41:58 -0300

    desafio curso Java Spring - Alura

---
+++ src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
@@ -0,0 +1,45 @@
+package br.com.alura.TabelaFipe.principal;
+
+import br.com.alura.TabelaFipe.model.Dados;
+import br.com.alura.TabelaFipe.service.ConsumoApi;
+import br.com.alura.TabelaFipe.service.ConverteDados;
+
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner leitura = new Scanner(System.in);
+
+    private ConsumoApi consumo = new ConsumoApi();
+
+    private ConverteDados conversor = new ConverteDados();
+
+    private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
+    public void exibeMenu() {
+        var menu = """
+                *** OPÇÕES ***
+                Carro
+                Moto
+                Caminhão
+                
+                Digite uma das opções para consultar: 
+                
+                """;
+
+        System.out.println(menu);
+        var opcao = leitura.nextLine();
+        String endereco;
+
+        if(opcao.toLowerCase().contains("carr")) {
+            endereco = URL_BASE + "carros/marcas";
+        } else if (opcao.toLowerCase().contains("mot")) {
+            endereco = URL_BASE + "motos/marcas";
+        } else {
+            endereco = URL_BASE + "caminhoes/marcas";
+        }
+
+        var json = consumo.obterDados(endereco);
+        System.out.println(json);
+        var marcas = conversor.obterLista(json, Dados.class);
+    }
+}
